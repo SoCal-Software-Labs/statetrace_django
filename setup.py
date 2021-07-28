98f76018b370e4bc2f8394cdf277925c4f78b4bb
Kyle <me@khanson.io>
2021-07-28 13:36:09 -0700

    Setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="statetrace_django",
     version="0.0.2",
-    packages=["statetrace_django", "statetrace_django.migrations"],
+    packages=setuptools.find_packages(),
     license="LICENSE.txt",
     description="Statetrace for Django",
     install_requires=["Django >= 3.0.0"],
