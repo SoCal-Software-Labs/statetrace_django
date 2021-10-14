0a973926a1a04e97ef9795f7b2611ebce7ffab99
kyle <me@khanson.io>
2021-10-14 16:32:05 -0700

    update to json

--- statetrace_django/models.py
+++ statetrace_django/models.py
@@ -22,21 +22,7 @@ class Annotation(models.Model):
     kind = models.TextField()
 
     meta = JSONField(null=True)
-
-    action_session_id = models.IntegerField(null=True)
-    action_url = models.TextField(null=True)
-    action_method = models.TextField(null=True)
-    action_version = models.TextField(null=True)
-    action_parent_id = models.TextField(null=True)
-    action_length_ms = models.IntegerField(null=True)
-
-    session_client_user_agent = models.TextField(null=True)
-    session_application_type = models.TextField(null=True)
-    session_application_id = models.TextField(null=True)
-    session_actor_id = models.TextField(null=True)
-    session_actor_full_name = models.TextField(null=True)
-    session_actor_email = models.TextField(null=True)
-    session_actor_avatar = models.TextField(null=True)
+    payload = JSONField(null=True)
 
     class Meta:
         db_table = "statetrace_annotations"
@@ -59,12 +45,14 @@ class Annotation(models.Model):
             id=new_id(),
             timestamp=timestamp or timezone.now(),
             meta=meta,
-            session_actor_id=actor_id and str(actor_id),
-            session_actor_full_name=actor_full_name,
-            session_actor_email=actor_email,
-            session_actor_avatar=actor_avatar,
-            session_client_user_agent=client_user_agent,
-            session_application_id=application_id,
+            payload=dict(
+                session_actor_id=actor_id and str(actor_id),
+                session_actor_full_name=actor_full_name,
+                session_actor_email=actor_email,
+                session_actor_avatar=actor_avatar,
+                session_client_user_agent=client_user_agent,
+                session_application_id=application_id
+            )
         )
 
     @classmethod
@@ -85,11 +73,13 @@ class Annotation(models.Model):
             timestamp=timestamp or timezone.now(),
             action_session_id=action_session_id,
             meta=meta,
-            action_url=action_url,
-            action_method=action_method,
-            action_version=action_version,
-            action_length_ms=action_length_ms,
-            action_parent_id=action_parent_id
+            payload=dict(
+                action_url=action_url,
+                action_method=action_method,
+                action_version=action_version,
+                action_length_ms=action_length_ms,
+                action_parent_id=action_parent_id
+            )
         )
 
     @classmethod
